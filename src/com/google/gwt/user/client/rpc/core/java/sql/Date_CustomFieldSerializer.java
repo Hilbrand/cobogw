fff78d8f2ec3dc4ff3c704e991d8e86c3bedb96a
com.bouwkamp.gwt <com.bouwkamp.gwt@384b1497-1a36-0410-818a-b32f2d072a7a>
2007-12-11 22:48:43 +0000

    removed spaces from source file.

--- src/com/google/gwt/user/client/rpc/core/java/sql/Date_CustomFieldSerializer.java
+++ src/com/google/gwt/user/client/rpc/core/java/sql/Date_CustomFieldSerializer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Hilbrand Bouwkamp, hs@bouwkamp.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -25,7 +25,7 @@ import java.sql.Date;
  * Custom field serializer for {@link java.sql.Date}.
  */
 public final class Date_CustomFieldSerializer {
-  public static void deserialize(SerializationStreamReader streamReader, 
+  public static void deserialize(SerializationStreamReader streamReader,
       Date instance) throws SerializationException {
     // No fields
   }
