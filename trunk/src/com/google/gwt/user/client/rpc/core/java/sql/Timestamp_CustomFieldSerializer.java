ae15435721831614f8944808f4a03306f04bab15
com.bouwkamp.gwt <com.bouwkamp.gwt@384b1497-1a36-0410-818a-b32f2d072a7a>
2007-12-11 22:48:43 +0000

    removed spaces from source file.

--- trunk/src/com/google/gwt/user/client/rpc/core/java/sql/Timestamp_CustomFieldSerializer.java
+++ trunk/src/com/google/gwt/user/client/rpc/core/java/sql/Timestamp_CustomFieldSerializer.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 Hilbrand Bouwkamp, hs@bouwkamp.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -25,7 +25,7 @@ import java.sql.Timestamp;
  * Custom field serializer for {@link java.sql.Timestamp}.
  */
 public class Timestamp_CustomFieldSerializer {
-  public static void deserialize(SerializationStreamReader streamReader, 
+  public static void deserialize(SerializationStreamReader streamReader,
       Timestamp instance) throws SerializationException {
     instance.setNanos(streamReader.readInt());
   }
@@ -41,4 +41,3 @@ public class Timestamp_CustomFieldSerializer {
     streamWriter.writeInt(instance.getNanos());
   }
 }
- 
