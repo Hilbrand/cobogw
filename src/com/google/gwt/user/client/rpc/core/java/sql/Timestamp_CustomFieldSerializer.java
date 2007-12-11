757a098a8518b666afc8d4203eb92276159cf5d9
com.bouwkamp.gwt <com.bouwkamp.gwt@384b1497-1a36-0410-818a-b32f2d072a7a>
2007-12-11 19:40:54 +0000

    Fixed nano support.

--- src/com/google/gwt/user/client/rpc/core/java/sql/Timestamp_CustomFieldSerializer.java
+++ src/com/google/gwt/user/client/rpc/core/java/sql/Timestamp_CustomFieldSerializer.java
@@ -27,7 +27,7 @@ import java.sql.Timestamp;
 public class Timestamp_CustomFieldSerializer {
   public static void deserialize(SerializationStreamReader streamReader, 
       Timestamp instance) throws SerializationException {
-    // No fields
+    instance.setNanos(streamReader.readInt());
   }
 
   public static Timestamp instantiate(SerializationStreamReader streamReader)
@@ -38,6 +38,7 @@ public class Timestamp_CustomFieldSerializer {
   public static void serialize(SerializationStreamWriter streamWriter,
       Timestamp instance) throws SerializationException {
     streamWriter.writeLong(instance.getTime());
+    streamWriter.writeInt(instance.getNanos());
   }
 }
  
