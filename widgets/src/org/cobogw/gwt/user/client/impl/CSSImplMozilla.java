04c5453b08416ccf9e463a64c241effcd1b7c471
hs@bouwkamp.com <hs@bouwkamp.com@384b1497-1a36-0410-818a-b32f2d072a7a>
2009-03-08 17:24:52 +0000

    - Fixed Button support for Firefox 2.0.

--- widgets/src/org/cobogw/gwt/user/client/impl/CSSImplMozilla.java
+++ widgets/src/org/cobogw/gwt/user/client/impl/CSSImplMozilla.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Hilbrand Bouwkamp, hs@bouwkamp.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -20,7 +20,27 @@ import com.google.gwt.dom.client.Element;
 public class CSSImplMozilla extends CSSImpl {
 
   @Override
+  public void setInlineBlock(Element element) {
+    if (detectFirefoxVersion() >= 3) {
+      element.getStyle().setProperty("display", "inline-block");
+    } else {
+      element.getStyle().setProperty("display", "-moz-inline-box");
+    }
+  }
+
+  @Override
   public void setSelectable(Element e, boolean selectable) {
     e.getStyle().setProperty("MozUserSelect", selectable ? "" : "none");
   }
+
+  /**
+   * Returns the version of Internet Explorer
+   *
+   * @return Version of Internet Explorer
+   */
+  private native float detectFirefoxVersion() /*-{
+    var index = navigator.userAgent.indexOf("Firefox") + 8;
+    if (index == -1) return 3.0;
+    return parseFloat(navigator.userAgent.substring(index));
+  }-*/;
 }
